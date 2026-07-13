16158c6aa4eb13a4288643f5e1b4d70aa257fa58
tuannv <ninhvantuan092000@gmail.com>
2026-07-13 10:23:49 +0700

    fix build

---
+++ api/index.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from '../src/common/filters/http-exception.filter';
+
+let cachedServer: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Enable global prefix
+  app.setGlobalPrefix('api');
+
+  // Set global ValidationPipe with production settings
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Register global HTTP exception filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.init();
+  return app.getHttpAdapter().getInstance();
+}
+
+export default async (req: any, res: any) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  cachedServer(req, res);
+};
